8beeee82426fbe8fc5a40332c9a99963f0799446
yMilitão <arthurmeloyt@gmail.com>
2021-06-01 17:42:57 -0300

    second commit

---
+++ 3-CriandoVariaveisPontoFlutuante/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3_CriandoVariaveisPontoFlutuante
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Criando variaveis Ponto futuante");
+            
+            double salario;
+            salario = 1200.70;
+            Console.WriteLine(salario);
+
+
+            Console.WriteLine("Aperte enter para finalizar a execução");
+            Console.ReadLine();
+        }
+    }
+}
