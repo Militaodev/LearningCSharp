520423097edc16e8044dcf9fa36c953340e31a13
yMilitão <arthurmeloyt@gmail.com>
2021-06-01 17:01:22 -0300

    first commit

---
+++ 2-CriandoVariaveis/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2_CriandoVariaveis
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Executando 2-Criando Variaveis");
+
+            int idade;
+            idade = 32;
+            Console.WriteLine(idade);
+            idade = 10;
+            Console.WriteLine(idade);
+
+            Console.WriteLine("Tecle enter para Fechar o console");
+            Console.ReadLine();
+        }
+    }
+}
